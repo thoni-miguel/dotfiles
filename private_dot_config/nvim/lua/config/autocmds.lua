900fd689e105d4144d1ad43bf2e50cf4fae54a95
thoni-miguel <thonimigueldev@gmail.com>
2025-04-29 21:28:17 -0300

    Add highlighting syntax to neovim whenever editing chezmoi files

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,36 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = {
+    "*.tmpl",
+  },
+  callback = function(args)
+    local fname = vim.fn.fnamemodify(args.file, ":t")
+    local ftmap = {
+      ["dot_zshrc.tmpl"] = "zsh",
+      ["dot_bashrc.tmpl"] = "bash",
+      ["Microsoft.PowerShell_profile.ps1.tmpl"] = "ps1",
+      ["dot_wezterm.lua.tmpl"] = "lua",
+    }
+
+    local ft = ftmap[fname]
+
+    if not ft then
+      -- fallback for extensions
+      if fname:match("%.lua%.tmpl$") then
+        ft = "lua"
+      elseif fname:match("%.bash%.tmpl$") then
+        ft = "bash"
+      elseif fname:match("%.ps1%.tmpl$") then
+        ft = "ps1"
+      elseif fname:match("%.zsh%.tmpl$") then
+        ft = "zsh"
+      end
+    end
+
+    if ft then
+      vim.bo.filetype = ft
+    end
+  end,
+})
